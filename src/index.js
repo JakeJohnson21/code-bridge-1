848ec747587153896c0dc5252561eda3a5fc16f3
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-09-06 02:50:05 -0500

    add browserROuter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="https://jakejohnson21.github.io/code-bridge-1/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
